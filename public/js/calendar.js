0746992ff10126892122a649ebb4a9567de848c0
jmccaslin21 <80499963+JMcCaslinDev@users.noreply.github.com>
2024-04-30 15:06:35 -0700

    updating calendar.js to fix calendar weekday divs

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-
   var navbarHeight = document.querySelector('nav').offsetHeight;
   var mainContent = document.getElementsByClassName('body');
   document.body.style.paddingTop = navbarHeight + 'px';
@@ -9,21 +8,13 @@ document.addEventListener('DOMContentLoaded', function() {
   const nextWeekButton = document.getElementById('next-week');
   const saveButton = document.getElementById('save-button');
 
-  // let specificDate = moment("2024-03-24");
-  // let currentWeekNumber = specificDate.isoWeek();
-
   let currentWeekNumber = moment().isoWeek();
-  console.log("\nCurrentWeekNumber ", currentWeekNumber, "\n")
-  let currentStartDate = ''; // To store the current start date of the week
-  let currentEndDate = ''; // To store the current end date of the week
-  let userRecipes = []; // This will hold the fetched recipes
+  console.log("\nCurrentWeekNumber ", currentWeekNumber, "\n");
+  let currentStartDate = '';
+  let currentEndDate = '';
+  let userRecipes = [];
 
-  if (fetchAndPopulateRecipes()){
-    console.log("\nTrying to set recipes for page load dropdown lists.\n")
-    userRecipes = fetchAndPopulateRecipes()
-  }
-    
-  
+  fetchAndPopulateRecipes();
 
   // Function to clear previous meal slots
   function clearMealSlots() {
@@ -32,72 +23,65 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }
 
-
   // Function to create a meal tag
   function createMealTag(dayIndex, meal) {
     const mealTag = document.createElement('div');
     mealTag.className = 'meal-tag';
     mealTag.setAttribute('data-day-index', dayIndex);
-  
-    // Create a dropdown for selecting recipes
+
     const recipeSelect = document.createElement('select');
     recipeSelect.className = 'meal-recipe-select';
-  
-    // Add a default "not selected" option
+
     const defaultOption = document.createElement('option');
     defaultOption.value = '';
     defaultOption.textContent = 'Pick a meal';
     defaultOption.selected = true;
     recipeSelect.appendChild(defaultOption);
-  
+
     console.log("Creating meal tag for day index:", dayIndex);
     console.log("Meal object:", meal);
 
     console.log("\nuserRecipes: ", userRecipes, "\n");
-    // Add recipe options to the dropdown
-    userRecipes.forEach(recipe => {
-      const option = document.createElement('option');
-      option.value = recipe.id;
-      option.textContent = recipe.recipeName;
-      recipeSelect.appendChild(option);
-      console.log("Added recipe option:", recipe.recipeName);
-    });
-  
-    // Set the selected recipe in the dropdown based on the meal's recipeId (if available)
+
+    if (Array.isArray(userRecipes)) {
+      userRecipes.forEach(recipe => {
+        const option = document.createElement('option');
+        option.value = recipe.id;
+        option.textContent = recipe.recipeName;
+        recipeSelect.appendChild(option);
+        console.log("Added recipe option:", recipe.recipeName);
+      });
+    }
+
     if (meal && meal.recipeId) {
       recipeSelect.value = meal.recipeId;
       console.log("Selected recipe ID:", meal.recipeId);
     }
 
-  
-    // Create a time input
     const timeInput = document.createElement('input');
     timeInput.type = 'time';
     timeInput.className = 'meal-time-input';
-  
-    // Set the time input value based on the meal's timeSlot
+
     const timeSlot = new Date(meal.timeSlot);
     const hours = timeSlot.getHours().toString().padStart(2, '0');
     const minutes = timeSlot.getMinutes().toString().padStart(2, '0');
     timeInput.value = `${hours}:${minutes}`;
-  
-    // Create a remove button for the meal tag
+
     const removeBtn = document.createElement('button');
     removeBtn.textContent = 'x';
     removeBtn.className = 'meal-remove-btn';
     removeBtn.onclick = function() {
       mealTag.remove();
     };
-  
-    // Adjust the DOM structure for the meal tag to meet the new layout requirements
+
     const timeAndRemoveContainer = document.createElement('div');
     timeAndRemoveContainer.className = 'time-remove-container';
     timeAndRemoveContainer.appendChild(timeInput);
     timeAndRemoveContainer.appendChild(removeBtn);
-  
+
     mealTag.appendChild(recipeSelect);
-    mealTag.appendChild(timeAndRemoveContainer); // Append the new container
-  
+    mealTag.appendChild(timeAndRemoveContainer);
+
     const mealSlotContainer = document.getElementById(`meal-slot-${dayIndex}`);
     if (mealSlotContainer) {
       mealSlotContainer.appendChild(mealTag);
@@ -106,147 +90,131 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   }
 
-
   // Fetch recipes from the backend and populate the dropdowns
   async function fetchAndPopulateRecipes() {
     try {
       const response = await fetch('/api/user-recipes');
       userRecipes = await response.json();
-      console.log("Fetched user recipes:", userRecipes); 
-      // Call a function to populate the recipe selects if needed
+      console.log("Fetched user recipes:", userRecipes);
     } catch (error) {
       console.error('Failed to fetch recipes', error);
     }
   }
-
 
   // Fetch data from the backend to initialize the calendar
   async function fetchDataForWeek(weekNumber) {
     console.log("\ninside fetch data for week function\n");
     const response = await fetch(`/api/week-data?week=${weekNumber}`);
     const data = await response.json();
-    console.log("Data fetched: ", data); // To see the actual fetched data
+    console.log("Data fetched: ", data);
     if (data) {
-      currentStartDate = data.startString; // Update the current start date
-      currentEndDate = data.endString; // Update the current end date
+      currentStartDate = data.startString;
+      currentEndDate = data.endString;
       console.log("\ndata.startString: ", data.startString, "\n");
       console.log("\ndata.endString: ", data.endString, "\n");
-      updateWeekLabel(currentStartDate, currentEndDate); // Update the week label
+      updateWeekLabel(currentStartDate, currentEndDate);
     }
-    return data; // Make sure to return the data
+    return data;
   }
-
-
-
 
   // Function to update the week label with the start and end dates
   function updateWeekLabel(startDate, endDate) {
-    currentStartDate = startDate; // Update the current start date
-    currentEndDate = endDate; // Update the current end date
+    currentStartDate = startDate;
+    currentEndDate = endDate;
     weekLabel.textContent = `${startDate}  -  ${endDate}`;
   }
 
+  // Populate the calendar with the meal tags from the database of users chosen meals
+  function populateCalendar(mealData) {
+    console.log("\nEntered populateCalendar function\n");
 
-  // Populate the calendar with the meal tags from the database of users choosen meals
-function populateCalendar(mealData) {
-  console.log("\nEntered populateCalendar function\n");
-  
-  clearMealSlots();
-  const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-  
-  const mealsByDay = {};
-  mealData.forEach(meal => {
-    const date = new Date(meal.timeSlot);
-    const dayIndex = date.getDay();
-    const dayName = days[dayIndex].toLowerCase();
-    if (!mealsByDay[dayName]) {
-      mealsByDay[dayName] = [];
-    }
-    mealsByDay[dayName].push(meal);
-  });
-  
-  days.forEach((day, index) => {
-    const mealSlot = document.getElementById(`meal-slot-${index}`);
-    console.log(mealSlot);
-    const meals = mealsByDay[day.toLowerCase()] || [];
-    meals.forEach(meal => {
-      createMealTag(index, meal);
-    });
-  });
-}
+    clearMealSlots();
+    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-
-// Function to send meal data to the server
-async function saveCalendar() {
-  const meals = [];
-  document.querySelectorAll('.day-column').forEach((column, index) => {
-    const day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][new Date(currentStartDate).getDay() + index % 7];
-    const date = new Date(currentStartDate);
-    date.setDate(date.getDate() + index); // Adjust date based on the day of the week
-    const dateString = date.toISOString().split('T')[0]; // Format date as string
-
-    column.querySelectorAll('.meal-tag').forEach(tag => {
-      const recipeSelect = tag.querySelector('.meal-recipe-select');
-      const timeInput = tag.querySelector('.meal-time-input');
-      if (recipeSelect.value) { // Only add meals with a selected recipe
-        meals.push({
-          recipeId: recipeSelect.value,
-          timeSlot: `${dateString} ${timeInput.value}:00` // Combine date and time
-        });
+    const mealsByDay = {};
+    mealData.forEach(meal => {
+      const date = new Date(meal.timeSlot);
+      const dayIndex = date.getDay();
+      const dayName = days[dayIndex].toLowerCase();
+      if (!mealsByDay[dayName]) {
+        mealsByDay[dayName] = [];
       }
-    });
-  });
-
-  // Send the meal data to the server
-  try {
-    const response = await fetch('/weekRecipes', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        weekNumber: currentWeekNumber,
-        startDate: currentStartDate,
-        endDate: currentEndDate,
-        meals: meals
-      })
+      mealsByDay[dayName].push(meal);
     });
 
-    if (response.ok) {
-      console.log('Meals saved successfully');
-      // Additional success handling if needed
-      // Call the function to update the shopping list
-      console.log("\ncurrentStartDate: ", currentStartDate, "\n");
-      console.log("\ncurrentEndDate: ", currentEndDate, "\n");
-      updateShoppingList(currentStartDate, currentEndDate);
-    } else {
-      throw new Error('Failed to save meals');
-    }
-  } catch (error) {
-    console.error('Error saving meals:', error);
-    // Additional error handling if needed
+    days.forEach((day, index) => {
+      const mealSlot = document.getElementById(`meal-slot-${index}`);
+      console.log(mealSlot);
+      const meals = mealsByDay[day.toLowerCase()] || [];
+      meals.forEach(meal => {
+        createMealTag(index, meal);
+      });
+    });
   }
-}
+
+  // Function to send meal data to the server
+  async function saveCalendar() {
+    const meals = [];
+    document.querySelectorAll('.day-column').forEach((column, index) => {
+      const day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][new Date(currentStartDate).getDay() + index % 7];
+      const date = new Date(currentStartDate);
+      date.setDate(date.getDate() + index);
+      const dateString = date.toISOString().split('T')[0];
+
+      column.querySelectorAll('.meal-tag').forEach(tag => {
+        const recipeSelect = tag.querySelector('.meal-recipe-select');
+        const timeInput = tag.querySelector('.meal-time-input');
+        if (recipeSelect.value) {
+          meals.push({
+            recipeId: recipeSelect.value,
+            timeSlot: `${dateString} ${timeInput.value}:00`
+          });
+        }
+      });
+    });
+
+    try {
+      const response = await fetch('/weekRecipes', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          weekNumber: currentWeekNumber,
+          startDate: currentStartDate,
+          endDate: currentEndDate,
+          meals: meals
+        })
+      });
+
+      if (response.ok) {
+        console.log('Meals saved successfully');
+        console.log("\ncurrentStartDate: ", currentStartDate, "\n");
+        console.log("\ncurrentEndDate: ", currentEndDate, "\n");
+        updateShoppingList(currentStartDate, currentEndDate);
+      } else {
+        throw new Error('Failed to save meals');
+      }
+    } catch (error) {
+      console.error('Error saving meals:', error);
+    }
+  }
 
   // Add the save button event listener
   saveButton.addEventListener('click', saveCalendar);
 
-  
-
-
   // Navigate to previous or next week
   function navigateWeeks(weekNumber) {
-    console.log("\nnavigateWeeks_weekNumber: ", weekNumber, "\n" )
+    console.log("\nnavigateWeeks_weekNumber: ", weekNumber, "\n");
     fetchDataForWeek(weekNumber)
       .then(data => {
-        console.log("navigateweeks_data: ", data, "\n")
-        console.log("\nnavigateweeks_data.startString: ", data.startString, "\n")
-        assignDatesToDays(data.startString); // Assign the new dates to the day columns
-        populateCalendar(data.meals); // Populate the calendar with meals for these dates
+        console.log("navigateweeks_data: ", data, "\n");
+        console.log("\nnavigateweeks_data.startString: ", data.startString, "\n");
+        assignDatesToDays(data.startString);
+        populateCalendar(data.meals);
       })
       .catch(error => console.error('Failed to fetch week data', error));
   }
-
 
   // Add event listeners to week navigation buttons
   prevWeekButton.addEventListener('click', () => {
@@ -260,49 +228,47 @@ async function saveCalendar() {
   });
 
   // Initial calendar setup for the current week
-  navigateWeeks(currentWeekNumber); // This will fetch and display the current week
+  navigateWeeks(currentWeekNumber);
 
- // Event listener for the Add Meal buttons
-document.querySelectorAll('.add-meal-button').forEach(button => {
-  button.addEventListener('click', function() {
-    const dayIndex = this.getAttribute('data-day');
-    const defaultMeal = {
-      recipeId: '',
-      timeSlot: ''
-    };
-    createMealTag(dayIndex, defaultMeal);
+  // Event listener for the Add Meal buttons
+  document.querySelectorAll('.add-meal-button').forEach(button => {
+    button.addEventListener('click', function() {
+      const dayIndex = this.getAttribute('data-day');
+      const defaultMeal = {
+        recipeId: '',
+        timeSlot: ''
+      };
+      createMealTag(dayIndex, defaultMeal);
+    });
   });
-});
 
+  // assignDatesToDays function
+  function assignDatesToDays(startDate) {
+    console.log("Inside assignDatesToDays function:");
 
-// assignDatesToDays function
-function assignDatesToDays(startDate) {
-  console.log("Inside assignDatesToDays function:");
-  
-  const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-  const startOfTheWeek = new Date(startDate);
+    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    const startOfTheWeek = new Date(startDate);
 
-  days.forEach((day, index) => {
-    let currentDay = new Date(startOfTheWeek);
-    currentDay.setDate(currentDay.getDate() + index);
-    let currentDayString = currentDay.getFullYear() + '-' +
-      ('0' + (currentDay.getMonth() + 1)).slice(-2) + '-' + 
-      ('0' + currentDay.getDate()).slice(-2);
+    days.forEach((day, index) => {
+      let currentDay = new Date(startOfTheWeek);
+      currentDay.setDate(currentDay.getDate() + index);
+      let currentDayString = currentDay.getFullYear() + '-' +
+        ('0' + (currentDay.getMonth() + 1)).slice(-2) + '-' +
+        ('0' + currentDay.getDate()).slice(-2);
 
-    const dayColumnId = 'day-' + day.toLowerCase();
-    console.log("Looking for dayColumn with ID:", dayColumnId);
+      const dayColumnId = 'day-' + day.toLowerCase();
+      console.log("Looking for dayColumn with ID:", dayColumnId);
 
-    const dayColumn = document.getElementById(dayColumnId);
-    if (dayColumn) {
-      console.log("dayColumn found:", dayColumn);
-      dayColumn.setAttribute('data-date', currentDayString);
-      console.log(currentDayString);
-    } else {
-      console.log("No element found for day:", day);
-    }
-  });
-}
-
+      const dayColumn = document.getElementById(dayColumnId);
+      if (dayColumn) {
+        console.log("dayColumn found:", dayColumn);
+        dayColumn.setAttribute('data-date', currentDayString);
+        console.log(currentDayString);
+      } else {
+        console.log("No element found for day:", day);
+      }
+    });
+  }
 
   // Function to update the shopping list
   async function updateShoppingList(startDate, endDate) {
@@ -319,8 +285,4 @@ function assignDatesToDays(startDate) {
       console.error('Error updating shopping list:', error);
     }
   }
-
-
-  // Call this function on page load to populate the recipe dropdowns
-  fetchAndPopulateRecipes();
 });
